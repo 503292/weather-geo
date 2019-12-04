f2a957d91839161490edea9e2591129de666e995
Kalysh <503292@ukr.net>
2019-12-04 14:06:53 +0200

    MUST-FIX _ favicon

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import './styles.css';
 import './spinner.css';
 
 import refs from './js/refs';
-import handlerWeather from "./js/handlerWeather"
+import handlerWeather from './js/handlerWeather';
 import getGeoPosition from './js/getGeoPosition';
 
 import 'pnotify/dist/PNotifyBrightTheme.css';
 
-
-// import getGeoPosition from './js/getGeoPosition'
-
-
+import './images/favicon.ico';
 
 refs.searchForm.addEventListener('submit', handlerWeather);
